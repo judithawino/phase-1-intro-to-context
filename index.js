e3fe5a7534b0bae12c2ea02121223e62f6aa7672
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-06-16 21:38:47 +0300

    created createEmployeeRecord and createEmployeeRecords

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+//1. using variable declaration to create and store a function
+let createEmployeeRecord = function(arr){
+    //2. return an object and populate its record from an Array
+    return {
+        //populates a firstName field from the 0th element
+        firstName:arr[0],
+        familyName:arr[1],
+        title:arr[2],
+        payPerHour:arr[3],
+        //initializing the following keys with an empty array
+        timeInEvents:[],
+        timeOutEvents:[]
+    }
+}
+
+let createEmployeeRecords= function(employeeData){
+    
+}
